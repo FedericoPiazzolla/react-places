94cbcac51ddac1ddee1135e7b7e7f268e7ad318f
FedericoPiazzolla <Federicop257@gmail.com>
2025-01-21 22:02:39 +0100

    add: modal button without rendering map

--- src/shared/components/UIElements/Modal.jsx
+++ src/shared/components/UIElements/Modal.jsx
@@ -1,33 +1,33 @@
-import React, { useRef } from "react";
-import { ReactDOM } from "react-dom";
+import React, { useRef, forwardRef } from "react";
+import  ReactDOM  from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
 import BackDrop from "./BackDrop";
 import "./style/Modal.css";
 
-const ModalOverlay = (props) => {
+const ModalOverlay = forwardRef((props, ref) => {
   const content = (
-    <div className={`modal ${props.className}`} style={props.style}>
+    <div ref={ref} className={`modal ${props.className}`} style={props.style}>
       <header className={`modal__header ${props.headerClass}`}>
         <h2>{props.header}</h2>
       </header>
-
       <form
         onSubmit={
-          props.onSubmit ? props.onSubmit : (event) => event.preventDefault
-        }>
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
         <div className={`modal__content ${props.contentClass}`}>
           {props.children}
         </div>
-
         <footer className={`modal__footer ${props.footerClass}`}>
           {props.footer}
         </footer>
       </form>
     </div>
   );
+
   return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
-};
+});
 
 const Modal = (props) => {
   const nodeRef = useRef(null);
