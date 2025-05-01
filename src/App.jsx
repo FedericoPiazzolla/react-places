b3b7782a5d38c462ff8dd7f92d87d4597e1a9869
FedericoPiazzolla <Federicop257@gmail.com>
2025-05-01 18:59:13 +0200

    start: auth page

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/:userId/places" end element={<UserPlaces />} />
           <Route path="/places/new" end element={<NewPlace />} />
           <Route path="/places/:placeId" end element={<UpdatePlace />} />
+          <Route path="/auth" end element={<Auth />} />
         </Routes>
       </main>
     </Router>
---
+++ src/user/pages/Auth.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import useForm from "../../shared/hooks/form-hook";
+import Button from "../../shared/components/FormElements/Button";
+
+import "../pages/style/Auth.css";
+
+const Auth = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      email: { value: "", isValid: false },
+      password: { value: "", isValid: false },
+    },
+    false
+  );
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs); // manderemo i dati al back end
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {/* Ogni input è collegato al reducer tramite onInput (che usa dispatch internamente) */}
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
+          onInput={inputHandler}
+          errorText="Please enter a valid email address."
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(8)]}
+          onInput={inputHandler}
+          errorText="Please enter a valid password (at least 8 characters)."
+        />
+
+        {/* Il bottone è disabilitato finché il form non è valido */}
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
